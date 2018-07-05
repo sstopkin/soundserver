8f55aa3236e5c14587970885b31e3e0abdc3cc34
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-07-06 00:51:37 +0600

    Fixed systemd init

--- web-ui/app.js
+++ web-ui/app.js
@@ -26,7 +26,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.load({ path: '.env.example' });
+dotenv.load({ path: '/var/soundserver/web-ui/.env.example' });
 
 /**
  * Controllers (route handlers).
