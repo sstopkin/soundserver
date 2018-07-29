97a105a2aebebe63ff9906d8d7752a17b3f8927b
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-07-30 00:21:01 +0600

    Added cputemp and cpufreq

--- web-ui/controllers/system.js
+++ web-ui/controllers/system.js
@@ -1,8 +1,14 @@
 const shell = require('shelljs');
+const fs = require('fs');
+var soundServerConfig = require('../config/soundserver.json');
 
 exports.getSystem = (req, res) => {
+  let cpuFreq = getCpuFreqData(soundServerConfig.cpuFreqDataPath);
+  let cpuTemp = getCpuTempData(soundServerConfig.cpuTempDataPath);
+  let systemData = [{'name':'cpuFreq (MHz)','value':cpuFreq}, {'name':'cpuTemp (°C)','value':cpuTemp}];
   res.render('system/system', {
-    title: 'System'
+    title: 'System',
+    systemData
   });
 };
 
@@ -10,4 +16,36 @@ exports.rebootSystem = (req, res) => {
   req.flash('success', { msg: 'Restarting system. Please wait..' });
   console.log("rebootSystem");
   res.redirect('/system');
+}
+
+function getCpuTempData(file){
+  let content = '';
+  // Check that the file exists locally
+  if(!fs.existsSync(file)) {
+    content = 'n/a'
+    console.log("File not found - " + file);
+  }
+  // The file *does* exist
+  else {
+    // Read the file and do anything you want
+    content = fs.readFileSync(file, 'utf-8');
+    content = [content.slice(0, 2), ',', content.slice(2)].join('');
+  }
+  return content;
+}
+
+function getCpuFreqData(file){
+  let content = '';
+  // Check that the file exists locally
+  if(!fs.existsSync(file)) {
+    content = 'n/a'
+    console.log("File not found - " + file);
+  }
+  // The file *does* exist
+  else {
+    // Read the file and do anything you want
+    content = fs.readFileSync(file, 'utf-8');
+    content = content.slice(0, -3);
+  }
+  return content;
 }
