a20c8576aa11be3da16e03ba199581e3573024b2
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-09-12 23:47:20 +0600

    Deleted gravatar

--- web-ui/models/User.js
+++ web-ui/models/User.js
@@ -11,8 +11,7 @@ const userSchema = new mongoose.Schema({
   tokens: Array,
 
   profile: {
-    name: String,
-    picture: String
+    name: String
   }
 }, { timestamps: true });
 
@@ -39,20 +38,6 @@ userSchema.methods.comparePassword = function comparePassword(candidatePassword,
   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
     cb(err, isMatch);
   });
-};
-
-/**
- * Helper method for getting user's gravatar.
- */
-userSchema.methods.gravatar = function gravatar(size) {
-  if (!size) {
-    size = 200;
-  }
-  if (!this.email) {
-    return `https://gravatar.com/avatar/?s=${size}&d=retro`;
-  }
-  const md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return `https://gravatar.com/avatar/${md5}?s=${size}&d=retro`;
 };
 
 const User = mongoose.model('User', userSchema);
