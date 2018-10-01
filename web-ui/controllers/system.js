827d8de13657ae376ab7c95a75244af71c60dc21
Sergey Stopkin <stopkin.sergey@gmail.com>
2018-10-01 19:48:34 +0600

    Added time parser

--- web-ui/controllers/system.js
+++ web-ui/controllers/system.js
@@ -41,15 +41,21 @@ exports.postGlobal = (req, res) => {
   }
 
   let date = req.body.date;
-  console.log("date- "+date);
+  let time = req.body.time;
+  console.log("date- "+ date + " time-" + time);
 
   if (!isValidDate(date)) {
     req.flash('errors', { msg: 'Некорректное значение поля date.' });
     return res.redirect('/system/global');
   }
 
+  if (!isValidTime(time)) {
+    req.flash('errors', { msg: 'Некорректное значение поля time.' });
+    return res.redirect('/system/global');
+  }
+
   writeSettings('global', 'date', date);
-  writeSettings('global', 'time', req.body.time);
+  writeSettings('global', 'time', time);
   writeSettings('global', 'timezone', req.body.timezone);
 
   shell.exec(soundServerConfig.scriptsDir+'/datetime.sh', function(code, stdout, stderr) {
@@ -149,9 +155,14 @@ function getParameterByName(type, paramName){
   return fs.readFileSync(soundServerConfig.configDir+'/'+type+'/'+paramName, 'utf8');
 }
 
+// https://stackoverflow.com/a/14892796
+function isValidTime(timeString) {
+    var timeFormat = /^(?:2[0-3]|[01][0-9]):[0-5][0-9]:[0-5][0-9]$/;
+    return timeFormat.test(strTime);
+}
+
 // https://stackoverflow.com/questions/6177975/how-to-validate-date-with-format-mm-dd-yyyy-in-javascript
-function isValidDate(dateString)
-{
+function isValidDate(dateString) {
     // First check for the pattern
     var regex_date = /^\d{4}\-\d{1,2}\-\d{1,2}$/;
 
